2ddefef447b2f63480d8bad472d053e7908e8e1b
Jacob Andersen <jacobma87@gmail.com>
2021-03-27 23:36:36 -0400

    beginning idea for study tool time keeping

---
+++ DateDemo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class DateDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello, it's: ");
+        System.out.println(new Date());
+    }
+    
+}
