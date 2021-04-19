17c49920b6010a16aea1d9cccbae147e4828dc8f
Jacob Andersen <jacobma87@gmail.com>
2021-04-18 23:14:26 -0400

    updated experiments with the Stream API

--- StreamTest.java
+++ StreamTest.java
@@ -3,8 +3,9 @@ import java.util.stream.Stream;
 public class StreamTest {
  
     public static void main(String[] args) {
-        Stream<String> s = Stream.of("duck","duck","duck","goose");
-        s.distinct()
+        Stream<String> s = Stream.of("duck","duck","duck","goose","people","people");
+        s.filter(x -> x.length() > 4)
+        .distinct()
         .forEach(System.out::println);
     }
 }
