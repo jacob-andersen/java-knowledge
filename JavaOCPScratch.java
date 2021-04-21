ebec5a58e9bee9d24ebf19e0ee3534af7a79ba6c
Jacob Andersen <jacobma87@gmail.com>
2021-04-20 22:47:45 -0400

    utility class to prepare for File questions

---
+++ JavaOCPScratch.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class JavaOCPScratch {
+    public static void main(String[] args) {
+        Locale locale = Locale.getDefault();
+        System.out.println(locale.GERMAN);
+        var zooFile1 = new File("/home/tiger/data/stripes.txt");
+        System.out.println(zooFile1.exists());
+        var buffer = new byte[1024];
+        
+    }
+    
+}
