a6a939d1cc4bbac461ce7e138e3aca2ea4822b33
Jacob Andersen <jacobma87@gmail.com>
2021-04-26 18:27:58 -0400

    playing with lambda expressions

--- FunWithDatatypes.java
+++ FunWithDatatypes.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class FunWithDatatypes {
 
   public static void main(String[] args) {
@@ -31,5 +33,11 @@ public class FunWithDatatypes {
     }
 
     System.out.println(s.strip());
-  }
+
+  Map<String, Integer> bunnies = new HashMap<>();
+  bunnies.put("long ear", 3);
+  bunnies.put("floppy", 8);
+  bunnies.put("hoppy", 1);
+  bunnies.forEach((k, v) -> System.out.println(k + " " + v));
+}
 }
