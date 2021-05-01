a5e6fc91894536bac0f93cd46c1ec97b62c83bed
Jacob Andersen <jacobma87@gmail.com>
2021-04-30 22:13:01 -0400

    playing around with static methods

--- StaticExample.java
+++ StaticExample.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 class StaticExample {
 
     static String message;
+    String message1 = "Yo Universe!";
+    private static final ArrayList<String> values = new ArrayList<>();
 
     static {
         message ="Hello World!";
@@ -16,9 +20,14 @@ class StaticExample {
     }
 
     public static void main(String[] args) {
+        values.add("changed");
+        System.out.println(values.get(0));
 
         StaticExample example = new StaticExample();
         example.show();
+
+        System.out.println(example.message1);
+
         
     }
     
