b2597510dd247538bfc27511016ec699ff6deeb9
Jacob Andersen <jacobma87@gmail.com>
2021-05-04 22:43:23 -0400

    removing vowels from word strings

---
+++ DisemvowelTrolls.java
@@ -0,0 +1,16 @@
+public class DisEmvowelTrolls {
+    public static void main(String[] args) {
+        System.out.println(disemVowel("This website is for losers LOL!"));        
+    }
+
+    public static String disemVowel (String str) {
+        String vowels = "aeiouAEIOU";
+        String cleanString = "";
+        for (int i=0;i<str.length()-1;i++) {
+            if (vowels.indexOf(str.charAt(i))<0) {
+                cleanString+=str.charAt(i);
+            }
+        }
+        return cleanString;
+    }
+}
