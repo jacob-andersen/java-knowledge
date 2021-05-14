0c4caa717b12dbbbe2e17dd62ceddcfcbf66bb45
Jacob Andersen <jacobma87@gmail.com>
2021-05-13 22:44:15 -0400

    first draft of Candy prices code challenge

---
+++ CandyPriceCodeChallenge.java
@@ -0,0 +1,15 @@
+/* Given an array of candy prices and a fixed dollar amount, determined how many pieces of candy can be
+   bought.
+
+   1. Sort the array in ascending order
+   2. Keep a running sum of candy prices added, as well as a counter of array items.
+   3. Increase counter while sum is smaller than dollar amount available
+   4. Once sum exceeds available money, return counter.
+
+*/
+
+public class CandyPriceCodeChallenge {
+    public static void main(String[] args) {
+        
+    }
+}
