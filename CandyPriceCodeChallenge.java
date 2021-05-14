70f42b576ea2950562b6d9393a55b032bea7c005
Jacob Andersen <jacobma87@gmail.com>
2021-05-13 23:04:50 -0400

    Candy Price code challenge updated

--- CandyPriceCodeChallenge.java
+++ CandyPriceCodeChallenge.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /* Given an array of candy prices and a fixed dollar amount, determined how many pieces of candy can be
    bought.
 
@@ -10,6 +12,22 @@
 
 public class CandyPriceCodeChallenge {
     public static void main(String[] args) {
-        
+
+        int count = 0;
+        int sum = 0;
+        int[] candyPrices = { 8, 4, 1, 3, 10, 11 };
+        int amount = 25;
+
+        Arrays.sort(candyPrices);
+
+        for (int elem : candyPrices) {
+            if (sum + elem < amount) {
+                sum += elem;
+                count++;
+            } else {
+                System.out.println(count);
+                break;
+            }
+        }
     }
 }
