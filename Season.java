fb2d351c2cb8cd6b30803c40e20ddb565392229f
Jacob Andersen <jacobma87@gmail.com>
2021-05-17 22:25:57 -0400

    updated enum example

--- Season.java
+++ Season.java
@@ -1,3 +1,15 @@
-public enum Season {
-    WINTER, SPRING, SUMMER, FALL    
+enum Season {
+    WINTER(5), SPRING(10), SUMMER(15), FALL(20);
+
+    private int value;
+
+    private Season(int value) {
+        this.value = value;
+    }
+
+    public static void main(String args[]) {
+        for (Season s : Season.values())
+            System.out.println(s + " " + s.value);
+
+    }
 }
