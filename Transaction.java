8cb960b5d6503fe45e2edea5043127e58f5706ac
Jacob Andersen <jacobma87@gmail.com>
2021-05-31 20:57:53 -0400

    added getters and setters to Transaction class

--- Transaction.java
+++ Transaction.java
@@ -1,18 +1,64 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 class Transaction {
     private String description;
     private Double amount;
     private LocalDate date;
-    
+
     Transaction(String description, Double amount) {
         this.description = description;
-        this.amount=amount;
+        this.amount = amount;
         this.date = LocalDate.now();
     }
 
+    
+
+    public String getDescription() {
+        return description;
+    }
+
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+    public Double getAmount() {
+        return amount;
+    }
+
+
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+
+
     public static void main(String[] args) {
-        Transaction t = new Transaction ("Milk", 3.99);
-        System.out.println(t.description + "    " + t.amount + "    " + t.date);
+        List<Transaction> trList = new ArrayList<Transaction>();
+        for (int i = 0; i < 10; i++) {
+            trList.add(new Transaction("Milk", 3.99));
+        }
+
+        for (Transaction t : trList) {
+            System.out.println(t.description + "    " + t.amount + "    " + t.date);
+        }
     }
 }
